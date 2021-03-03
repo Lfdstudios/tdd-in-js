f6b3ca818b06ff7e7cc585ff4fd34ece7b8aa75d
Lfdstudios <54958946+Lfdstudios@users.noreply.github.com>
2021-03-03 15:15:07 -0500

    basic functions and tests

--- index.js
+++ index.js
@@ -3,7 +3,64 @@ function add (input) {
             return result + input;
         })
     }
+function subtract (input) {
+    
+    try{
+    return operation(input, function (result, input) {
+            return result - input;
+        })
+     } catch{
+         return "cannot subtract two strings"
+     }
+    }
+function divide (input) {
 
+    return operation(input, function (result, input) {
+        
+        if (!Number.isFinite(result / input || input === 'error not finite')) {
+            return "error not finite";
+        }
+            return result / input;
+        });
+        
+        
+    }
+function multiple (input) {
+
+    return operation(input, function (result, input) {
+        
+        if (!Number.isFinite(result * input )) {
+            return "error not finite";
+        }
+            return result * input;
+        });
+        
+            
+    }
+function modulo (input) {
+
+    return operation(input, function (result, input) {
+        console.log(result, input)
+        if (!Number.isFinite(result % input )) {
+            return "error not finite";
+        }
+            return result % input;
+        });
+            
+                
+        }
+function pow (input) {
+
+    return operation(input, function (result, input) {
+        console.log(result, input)
+        if (!Number.isFinite(result ** input )) {
+            return "error not finite";
+        }
+            return result ** input;
+        });
+                    
+                        
+        }
 function operation(input, operator) {
     var result = input.shift();
     for (i = 0; i < input.length; i++) {
@@ -14,4 +71,4 @@ function operation(input, operator) {
 }
 
 
-module.exports = { add, operation}
+module.exports = { add, subtract, divide, multiple, modulo, pow, operation}
